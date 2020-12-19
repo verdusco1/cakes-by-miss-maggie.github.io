2b3d7cd7fbfde340d214e131f079cf24843ddead
Eduardo Verdusco <em.verdusco@gmail.com>
2020-12-19 10:32:06 -0800

    updated php code for form

--- contactform.php
+++ contactform.php
@@ -3,7 +3,7 @@
 if (isset($_POST['submit'])) {
     $name = $_POST['name'];
     $subject = $_POST['subject'];
-    $mailFrom = $_POST['mailFrom'];
+    $mailFrom = $_POST['mail'];
     $message = $_POST['message'];
 
     $mailTo = "eduardo.verdusco@yahoo.com";
